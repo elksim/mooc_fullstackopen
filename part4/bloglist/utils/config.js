ccbc991357ea284c1b20be3e7b4476584d42d594
Elliott Botha <elliottbotha@gmail.com>
2023-12-15 22:29:38 +1100

    part 6.4 done

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -6,6 +6,9 @@ const MONGODB_URI =
 	process.env.NODE_ENV == "test"
 		? process.env.TEST_MONGODB_URI
 		: process.env.MONGODB_URI;
+console.log('process.env.TEST_MONGODB_URI: ', process.env.TEST_MONGODB_URI);
+console.log('process.env.MONGODB_URI;: ', process.env.MONGODB_URI);
+console.log("setting MOGODB_URI to", MONGODB_URI);
 
 module.exports = {
 	MONGODB_URI,
